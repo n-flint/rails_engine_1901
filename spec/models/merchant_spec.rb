4125bb38462e1ffbf06e525a01bf13712a8f9796
Noah Flint <noah.flint24@gmail.com>
2019-05-02 14:48:32 -0600

    added validations to merchant_spec.rb

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe Merchant, type: :model do
   end
 
   describe 'relationships' do
-    it { should have_many :items }
-    it { should have_many :invoices }
+    it {should have_many :items }
+    it {should have_many :invoices }
+    it {should have_many(:customers).through(:invoices)}
   end
 end
